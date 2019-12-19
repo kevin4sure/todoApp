0d17df1bb0985ac9758fb825d222f3f93d45a7fc
Kamlesh Gupta <kamleshguptaslg@gmail.com>
2019-12-20 01:53:25 +0530

    CRUD - C operation done

--- ToDoPro/ToDoApp/urls.py
+++ ToDoPro/ToDoApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('',views.ViewList,name="all_tasks")
+    path('',views.ViewList,name="all_tasks"),
+    path('add/', views.CreateListItem,name='new_task')
 ]
--- ToDoPro/ToDoApp/views.py
+++ ToDoPro/ToDoApp/views.py
@@ -1,9 +1,24 @@
 from django.http import JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 import json
 # Create your views here.
 from .models import TaskList
 
 def ViewList(request):
-    all_tasks = serializers.serialize('json', TaskList.objects.all())
-    return JsonResponse({'all_tasks':json.loads(all_tasks)})
+    if request.method == 'GET':
+        all_tasks = serializers.serialize('json', TaskList.objects.all())
+        return JsonResponse({'success':True,'all_tasks':json.loads(all_tasks)})
+    return JsonResponse({})    
+
+@csrf_exempt
+def CreateListItem(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        try:
+            new_task = TaskList.objects.create(title=title)
+            new_task.save()
+        except Exception as e: return JsonResponse({'success':False,'message':'some error occurred while trying to save the data.','error_msg':e.__str__})    
+        all_tasks = serializers.serialize('json', TaskList.objects.all())
+        return JsonResponse({'success': True, 'all_tasks': json.loads(all_tasks)})
+
